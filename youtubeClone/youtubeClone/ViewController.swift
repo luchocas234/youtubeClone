0dc30cdf910ca12321371393cb9a246b628f1502
Luciano <lfcastro2911@gmail.com>
2022-10-21 20:43:52 -0300

    clase n2

---
+++ youtubeClone/youtubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtubeClone
+//
+//  Created by u633168 on 21/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
